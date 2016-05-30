41325ef2d17b596a4a26fb2c4867117873fe5e63
wangyongbin <1326058785@qq.com>
2016-05-30 20:53:22 +0800

    提交 test-ch1

---
+++ test-ch1/src/main/java/TestCh1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/5/30.
+ */
+public class TestCh1 {
+    public String testCh1(){
+        return "test ch1";
+    }
+}
---
+++ test-ch1/src/test/java/TestCh1Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/5/30.
+ */
+public class TestCh1Main {
+    public static void main(String[] args) {
+        System.out.println(new TestCh1().testCh1());
+    }
+}
